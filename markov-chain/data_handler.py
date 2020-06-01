b0a490746b81d039ea74699a3b65afe4cf57449e
Rachel <rachel.brabender@student.hpi.de>
2020-06-01 11:04:42 +0200

    experiments on data transformation (cube, sqt, log10) and introducing this also to the environment

--- markov-chain/data_handler.py
+++ markov-chain/data_handler.py
@@ -1,6 +1,7 @@
 import os
 import random
 import pandas as pd
+import numpy as np
 from typing import Tuple, List
 
 
@@ -20,13 +21,21 @@ class DataHandler:
             for f in files:
                 if f.endswith(".csv"):
                     file_path = os.path.join(root, f)
-                    dataframe = pd.read_csv(file_path)
-                    dataframe.columns = dataframe.columns.str.replace('\t', '')
-                    frames.append(dataframe)
+                    df = pd.read_csv(file_path)
+                    df.columns = df.columns.str.replace('\t', '')
+                    frames.append(df)
 
-        # combining all dataframes to one dataframe
+        # combining all df to one df
         self.data = pd.concat(frames, sort=False)[
             ['Optimal_Affected_Component', 'Optimal_Failure', 'Optimal_Rule', 'Optimal_Utility_Increase']]
+
+        # transform data
+        for index, row in self.data.iterrows():
+            np.seterr(divide='ignore')  # to turn off divided by zero warning
+            untransformed = row['Optimal_Utility_Increase']
+            self.data.loc[index, 'cube'] = np.power(untransformed, (1/3))
+            self.data.loc[index, 'sqt'] = np.sqrt(untransformed)
+            self.data.loc[index, 'log10'] = np.where(untransformed > 0, np.log10(untransformed), 0)
 
     def __create_component_failure_pairs(self) -> None:
         combinations = self.data.groupby(['Optimal_Affected_Component', 'Optimal_Failure']).size().reset_index().rename(
@@ -43,12 +52,24 @@ class DataHandler:
         else:
             return tuple(random.sample(self.component_failure_pairs, sample_size))
 
-    def get_reward(self, component_failure_pair: Tuple) -> float:
+    def _get_sample_row(self, component_failure_pair: Tuple) -> pd.DataFrame:
         component = component_failure_pair[0]
         failure = component_failure_pair[1]
         filtered = self.data[
             (self.data['Optimal_Affected_Component'] == component) & (self.data['Optimal_Failure'] == failure)]
-        return filtered.sample()['Optimal_Utility_Increase'].values[0]
+        return filtered.sample()
+
+    def get_reward(self, component_failure_pair: Tuple) -> float:
+        return self._get_sample_row(component_failure_pair)['Optimal_Utility_Increase'].values[0]
+
+    def get_reward_sqt(self, component_failure_pair: Tuple) -> float:
+        return self._get_sample_row(component_failure_pair)['sqt'].values[0]
+
+    def get_reward_cube(self, component_failure_pair: Tuple) -> float:
+        return self._get_sample_row(component_failure_pair)['cube'].values[0]
+
+    def get_reward_log10(self, component_failure_pair: Tuple) -> float:
+        return self._get_sample_row(component_failure_pair)['log10'].values[0]
 
 
 if __name__ == '__main__':
@@ -57,3 +78,6 @@ if __name__ == '__main__':
     print(component_failure_pairs)
     print(dataHandler.get_sample_component_failure_pairs(5))
     print(dataHandler.get_reward(component_failure_pairs[0]))
+    print(dataHandler.get_reward_sqt(component_failure_pairs[0]))
+    print(dataHandler.get_reward_cube(component_failure_pairs[0]))
+    print(dataHandler.get_reward_log10(component_failure_pairs[0]))
