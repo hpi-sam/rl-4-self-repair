0d2d809d734902398c581a6268a15fb653bf4b55
Nico Duldhardt <nduldhardt@gmail.com>
2020-06-21 11:14:38 +0200

    implemented elligibility traces

--- algorithm_analysis/algorithms.py
+++ algorithm_analysis/algorithms.py
@@ -1,9 +1,12 @@
+import itertools
+import matplotlib.pyplot as plt
+
 import gym
 import numpy as np
-import tqdm as tqdm
 
-from tqdm.autonotebook import tqdm
+from tqdm.auto import tqdm
 from algorithm_analysis.metrics import Metric
+from algorithm_analysis.plotting import plot_episode_length_over_time, plot_episode_reward_over_time
 from envs.broken_components import BrokenComponentsEnv
 from envs.data_handler import DataHandler
 
@@ -29,7 +32,7 @@ def epsilon_greedy(Q, epsilon, state):
 
 def run(alg, env, num_states, num_actions, episodes=1000,
         min_explore_rate=0.01, max_explore_rate=1, explore_rate_decay=0.005,
-        learning_rate=0.2, discount_rate=0.90):
+        learning_rate=0.2, discount_rate=0.90, trace_decay=0.9):
     """
 
     :param alg: Choose from 'qlearning' and 'sarsa'
@@ -46,39 +49,41 @@ def run(alg, env, num_states, num_actions, episodes=1000,
     episode_lengths = []
 
     for episode in tqdm(range(episodes)):
-
-        s = env.reset()
-        episode_length = 0
+        E = np.zeros((num_states, num_actions))
+        state = env.reset()
+        action = epsilon_greedy(Q, explore_rate, state)
         total_reward = 0
 
-        while True:
-            a = epsilon_greedy(Q, explore_rate, s)
+        for step in itertools.count(1):
+            E[state, action] += 1
 
-            s_next, reward, done, info = env.step(a)
+            next_state, reward, done, info = env.step(action)
 
             if alg == 'qlearning':
-                a_next = epsilon_greedy(Q, 0, s_next)
+                next_action = epsilon_greedy(Q, 0, next_state)
             elif alg == 'sarsa':
-                a_next = epsilon_greedy(Q, explore_rate, s_next)
+                next_action = epsilon_greedy(Q, explore_rate, next_state)
             else:
                 raise NotImplementedError(alg)
 
-            # update Q-Table for Q(s,a)
-            if not done:
-                Q[s, a] += learning_rate * (reward + (discount_rate * Q[s_next, a_next]) - Q[s, a])
-            else:
-                Q[s, a] += learning_rate * (reward - Q[s, a])
+            delta = reward + (discount_rate * Q[next_state, next_action]) - Q[state, action]
 
-            s = s_next
+            # update Q-Table for Q(s,a)
+            Q += learning_rate * delta * E
+            E = E * trace_decay * discount_rate
+            # else:
+            #     Q[state, action] += learning_rate * (reward - Q[state, action])
+
+            state = next_state
+            action = next_action
             total_reward += reward
-            episode_length += 1
 
             if done:
                 break
 
         # update episode metrics
         episode_explore_rates.append(explore_rate)
-        episode_lengths.append(episode_length)
+        episode_lengths.append(step)
         episode_rewards.append(total_reward)
 
         # model exploration rate decay.
@@ -87,20 +92,25 @@ def run(alg, env, num_states, num_actions, episodes=1000,
 
     env.close()
 
-    metrics = Metric(episodes, episode_lengths, episode_rewards, episode_explore_rates, learning_rate, discount_rate)
+    metrics = Metric(episodes, episode_lengths, episode_rewards, episode_explore_rates, learning_rate, discount_rate, trace_decay)
     return metrics
 
 
 if __name__ == '__main__':
     dataHandler = DataHandler()
-    broken_components = dataHandler.get_sample_component_failure_pairs(3)
+    broken_components = dataHandler.get_sample_component_failure_pairs(10)
     env = BrokenComponentsEnv(broken_components, reward_modus='raw')
     # env = gym.make('Taxi-v3')
 
-    learning_rate = 0.06
-    discount_rate = 0.06
+    learning_rate = 0.01
+    discount_rate = 0.9
 
-    metric = run('qlearning', env, env.observation_space.n, env.action_space.n, episodes=1000,
-                 learning_rate=learning_rate, discount_rate=discount_rate)
+    metric = run('qlearning', env, env.observation_space.n, env.action_space.n, episodes=200,
+                 learning_rate=learning_rate, discount_rate=discount_rate, trace_decay=0.9)
 
-    print(metric)
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5), constrained_layout=True, sharex=True,
+                            sharey='col')
+    plot_episode_length_over_time(axs[0], metric, smoothing_window=5)
+    plot_episode_reward_over_time(axs[1], metric, smoothing_window=5)
+
+    plt.show()
