559440cb5ffc0c4762181d219ee7d33d7339e781
Rachel <rachel.brabender@student.hpi.de>
2020-06-29 11:20:22 +0200

    Restructurings, adding non stationary data

--- envs/data_handler.py
+++ envs/data_handler.py
@@ -2,32 +2,34 @@ import os
 import sys
 import random
 import pandas as pd
+from envs.environments import check_environment
 import envs.data_utils as du
 
 
 class DataHandler:
 
-    def __init__(self, environment: str = 'Linear', take_component_id: bool = True):
+    def __init__(self, environment: str = 'Linear', take_component_id: bool = True, type: str = 'raw'):
         '''
         Initializes the Data Handler by loading data into the environment and select between using the componenent id or name.
 
         :param environment:
-            Choose between the version of the mRubis environment and its function for computing the utility_increase
-            'Linear' (default) | 'Saturating' | 'Combined' | 'Discontinuous'
-            or shifted data based on the component id
-            'Linear_Id_Shifted'
+            Choose between the version of the mRubis environment 'Linear' (default) | 'Saturating' | 'Combined' | 'Discontinuous'
+            or shifted data 'LinearShifted'
+            or non_stationary_data 'nonStationary'
         :param take_component_id:
             Choose compononent id or name. When take_component_id is false, you will take name.
+        :param type:
+            Choose between 'raw' (Default), 'sqt', 'cube', 'log10', 'ln', 'log2'
+
+        For all possible combinations of environment, id and type please have a look on the file 'environments.py'.
         '''
-        self.environment = environment
-        self.component_selector = 'Optimal_Affected_Component_Uid' if take_component_id else 'Optimal_Affected_Component'
+
+        self.environment, self.filename = check_environment(environment, take_component_id, type)
+        print(self.filename)
         self.data: pd.DataFrame = pd.DataFrame()
         self.component_failure_pairs = []
         self.__load_data()
         self.__create_component_failure_pairs()
-
-    def __prepared_data_file(self) -> str:
-        return 'prepared_data_' + self.environment + '.csv'
 
     def __load_transform_data(self) -> None:
         frames = []
@@ -35,7 +37,7 @@ class DataHandler:
         # searching for all csv files in the data directory and loading the data in multiple dataframes
         for root, dirs, files in os.walk('data'):
             for f in files:
-                if f.endswith(self.environment + '.csv'):
+                if f.endswith(self.environment[0] + '.csv'):
                     file_path = os.path.join(root, f)
                     df = pd.read_csv(file_path)
                     df.columns = df.columns.str.replace('\t', '')
@@ -49,16 +51,17 @@ class DataHandler:
         self.data = du.transform_data(self.data)
 
         # save transformed data this to environment as a csv file for quick reload in future
-        self.data.to_csv(self.__prepared_data_file())
+        self.data.to_csv(self.filename)
 
     def __load_data(self):
         try:
-            self.data = pd.read_csv(self.__prepared_data_file(), index_col=0)
+            self.data = pd.read_csv(self.filename, index_col=0)[[self.environment[1], 'Optimal_Failure', self.environment[2]]]
         except FileNotFoundError:
-            self.__load_transform_data()
+            print('Please restart.')
+            sys.exit(0)
 
     def __create_component_failure_pairs(self) -> None:
-        combinations = self.data.groupby([self.component_selector, 'Optimal_Failure']).size().reset_index().rename(
+        combinations = self.data.groupby([self.environment[1], 'Optimal_Failure']).size().reset_index().rename(
             columns={0: 'count'})
         del combinations['count']
         self.component_failure_pairs = [tuple(val) for val in combinations.values]
@@ -75,12 +78,12 @@ class DataHandler:
         else:
             return list(random.sample(self.component_failure_pairs, sample_size))
 
-    def get_reward(self, component_failure_pair, type: str ='raw') -> float:
+    def get_reward(self, component_failure_pair) -> float:
         component = component_failure_pair[0]
         failure = component_failure_pair[1]
         filtered = self.data[
-            (self.data[self.component_selector] == component) & (self.data['Optimal_Failure'] == failure)]
-        return filtered.sample()[type].values[0]
+            (self.data[self.environment[1]] == component) & (self.data['Optimal_Failure'] == failure)]
+        return filtered.sample()[self.environment[2]].values[0]
 
 
 if __name__ == '__main__':
@@ -92,18 +95,11 @@ if __name__ == '__main__':
     print('Get two samples:', dh.get_sample_component_failure_pairs(2))
     print('untransformed sampled:', dh.get_reward(component_failure_pairs[0]))
 
-    # Transformation
-    print('Square Root Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='sqt'))
-    print('Cube Root Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='cube'))
-    print('Log10 Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='log10'))
-    print('Ln Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='ln'))
-    print('Log2 Transformation sampled:', dh.get_reward(component_failure_pairs[0], type='log2'))
-
     # Shifting
-    ordering, shiftedData = du.shift_data(dh.data, 'cube', times=2)
-    result = du.perform_ttest(ordering, shiftedData, 'cube')
+    ordering, shiftedData = du.shift_data(dh.data, 'Optimal_Affected_Component_Uid', 'cube', times=2)
+    result = du.perform_ttest('Optimal_Affected_Component_Uid', ordering, shiftedData, 'cube')
     print('Perform T-Test on shifted data:', (len(result[result['pvalue']<0.025])))
 
     # Non-stationary series
     max_min_std_values = dh.data[['Optimal_Affected_Component_Uid', 'Optimal_Failure', 'raw']].groupby(['Optimal_Affected_Component_Uid', 'Optimal_Failure'])['raw'].agg([max, min, 'std']).reset_index()
-    print('Create AR non-stationary time series', du.create_non_stationary_data(max_min_std_values, du.ARCH, N=100))
+    print('Create AR non-stationary time series', du.create_non_stationary_data('Optimal_Affected_Component_Uid', 'raw', max_min_std_values, du.ARCH, N=100))
