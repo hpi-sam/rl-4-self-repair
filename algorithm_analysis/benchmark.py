c6b6b07f640f663b01e4ddf0fae7780f12c7819b
Hao Nguyen <haonguyen@steelix.local>
2020-07-01 13:17:45 +0200

    benchmark file with static broken components for benchmarkmarking

--- algorithm_analysis/benchmark.py
+++ algorithm_analysis/benchmark.py
@@ -1,7 +1,7 @@
 from envs.broken_components import DataHandler
 
 dh = DataHandler()
-component_nums = [4,7,11,18,29,45,66,93,124,155,182,204,220,230,238,242,245,247,248,250]
+component_nums = [4, 7, 11, 18, 29, 45, 66, 93, 124, 155, 182, 204, 220, 230, 238, 242, 245, 247, 248, 250]
 seed = 42
 
 BROKEN_COMPONENTS_LIST = [dh.get_sample_component_failure_pairs(component_num, seed) for component_num in component_nums]
