5ed9be45b0480ae7110b2212b9f65005233abd57
Hao Nguyen <haonguyen@steelix.local>
2020-07-09 12:33:43 +0200

    changed parameter names for better understanding and implemented train by episode

--- algorithm_analysis/a2c.py
+++ algorithm_analysis/a2c.py
@@ -44,19 +44,19 @@ class Model(tf.keras.Model):
 
 
 class A2CAgent:
-    def __init__(self, model, lr=7e-3, gamma=0.99, value_c=0.5, entropy_c=1e-4):
+    def __init__(self, model, learning_rate=7e-3, discount_rate=0.99, value_coefficient=0.5, entropy_coefficient=1e-4):
         # `gamma` is the discount factor; coefficients are used for the loss terms.
-        self.gamma = gamma
-        self.value_c = value_c
-        self.entropy_c = entropy_c
+        self.discount_rate = discount_rate
+        self.value_coefficient = value_coefficient
+        self.entropy_coefficient = entropy_coefficient
 
         self.model = model
         self.model.compile(
-            optimizer=RMSprop(lr=lr),
+            optimizer=RMSprop(lr=learning_rate),
             # Define separate losses for policy logits and value estimate.
             loss=[self._logits_loss, self._value_loss])
 
-    def train(self, env, batch_sz=64, updates=250):
+    def train_by_batches(self, env, batch_sz=64, updates=250):
         # Storage helpers for a single batch of data.
         actions = np.empty((batch_sz,), dtype=np.int32)
         rewards, dones, values = np.empty((3, batch_sz))
@@ -86,6 +86,38 @@ class A2CAgent:
             logging.debug("[%d/%d] Losses: %s" % (update + 1, updates, losses))
 
         return ep_rewards
+    
+    def train_by_episodes(self, env, episodes=250):
+        ep_rewards = [0.0]
+        step = 0
+        for episode in tqdm(range(episodes), desc='Episode Loop: '):
+            actions = np.empty((1,), dtype=np.int32)
+            rewards, dones, values = np.empty((3, 1))
+            observations = np.empty((1,) + np.array(env.masks[0]).shape)  # !!! change for our broken component env
+            next_obs = env.reset()
+            dones[step] = 0
+            while not dones[step]:
+                observations[step] = next_obs.copy()
+                actions[step], values[step] = self.model.action_value(next_obs[None, :])
+                next_obs, rewards[step], dones[step], _ = env.step(actions[step])
+
+                ep_rewards[-1] += rewards[step]
+                
+                _, next_value = self.model.action_value(next_obs[None, :])
+                returns, advs = self._returns_advantages(rewards, dones, values, next_value)
+                # A trick to input actions and advantages through same API.
+                acts_and_advs = np.concatenate([actions[:, None], advs[:, None]], axis=-1)
+                # Performs a full training step on the collected batch.
+                # Note: no need to mess around with gradients, Keras API handles it.
+                losses = self.model.train_on_batch(observations, [acts_and_advs, returns])
+                logging.debug("[%d/%d] Losses: %s" % (episode + 1, episodes, losses))
+        
+                if dones[step]:
+                    ep_rewards.append(0.0)
+                    next_obs = env.reset()
+                    logging.info("Episode: %03d, Reward: %03d" % (len(ep_rewards) - 1, ep_rewards[-2]))
+
+        return ep_rewards
 
     def test(self, env, render=False):
         obs, done, ep_reward = env.reset(), False, 0
@@ -102,7 +134,7 @@ class A2CAgent:
         returns = np.append(np.zeros_like(rewards), next_value, axis=-1)
         # Returns are calculated as discounted sum of future rewards.
         for t in reversed(range(rewards.shape[0])):
-            returns[t] = rewards[t] + self.gamma * returns[t + 1] * (1 - dones[t])
+            returns[t] = rewards[t] + self.discount_rate * returns[t + 1] * (1 - dones[t])
         returns = returns[:-1]
         # Advantages are equal to returns - baseline (value estimates in our case).
         advantages = returns - values
@@ -110,7 +142,7 @@ class A2CAgent:
 
     def _value_loss(self, returns, value):
         # Value loss is typically MSE between value estimates and returns.
-        return self.value_c * mean_squared_error(returns, value)
+        return self.value_coefficient * mean_squared_error(returns, value)
 
     def _logits_loss(self, actions_and_advantages, logits):
         # A trick to input actions and advantages through the same API.
@@ -127,4 +159,4 @@ class A2CAgent:
         entropy_loss = categorical_crossentropy(probs, probs)
         # We want to minimize policy and maximize entropy losses.
         # Here signs are flipped because the optimizer minimizes.
-        return policy_loss - self.entropy_c * entropy_loss
+        return policy_loss - self.entropy_coefficient * entropy_loss
