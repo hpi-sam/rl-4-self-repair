ce69b3b42b3ba0d11b0b6e73bd27902dd7d1b43c
Hao Nguyen <haonguyen@steelix.local>
2020-07-10 02:02:35 +0200

    added A2C metrics

--- algorithm_analysis/metrics.py
+++ algorithm_analysis/metrics.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import List
 
-
 @dataclass(frozen=True)
 class TabularMetric:
     episodes: int
@@ -11,3 +10,26 @@ class TabularMetric:
     learning_rate: float
     discount_rate: float
     trace_decay: float
+        
+        
+@dataclass(frozen=True)
+class A2C_MetricByBatch:
+    batch_size: int
+    updates: int
+    episode_lengths: List[int]
+    rewards: List[float]
+    learning_rate: float
+    discount_rate: float
+    value_coefficient: float
+    entropy_coefficient: float
+
+
+@dataclass(frozen=True)        
+class A2C_MetricByEpisodes:
+    episodes: int
+    episode_lengths: List[int]
+    rewards: List[float]
+    learning_rate: float
+    discount_rate: float
+    value_coefficient: float
+    entropy_coefficient: float
